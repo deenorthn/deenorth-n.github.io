58067e2ec6cfbd7765473518c25f31349fde7116
deenorthn <deenorth.naganathan@torontomu.ca>
2024-10-30 18:16:20 -0400

    Lab 5

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-var name = prompt("Please enter your name", "Name"); // Prompting the user to type in their name
-document.getElementByID("displayName").innerText = "Hello," + Name + "Welcome to my page!";
-
-function toggleImageSize() { // Enlarging the image
-	var image = document.getElementByID("profilePicture");
-	if (image.style.transform = "scale(2)") {
-		image.style.transform = "scale(1)";
-	} else {
-		image.style.transform = "scale(2)";
-	}
-{
-
-function highlightLink(link) {
-	link.style.color = "blue"; // Changes text color
-	link.style.backgroundcolor = "red"; // Changes background color
+// Prompt the user for their name and display it
+var userName = prompt("Please enter your name", "Name");
+if (userName) {
+    document.getElementById("displayName").innerText = "Hello, " + userName + "! Welcome to my page!";
 }
 
+// Toggle image size on click
+function toggleImageSize() {
+    var image = document.getElementById("profilePicture");
+    if (image.style.transform === "scale(2)") {
+        image.style.transform = "scale(1)";
+    } else {
+        image.style.transform = "scale(2)";
+    }
+}
+
+// Highlight link on mouseover
+function highlightLink(link) {
+    link.style.color = "blue";
+    link.style.backgroundColor = "red";
+}
+
+// Reset link style on mouseout
 function resetLink(link) {
-	link.style.color = ""; // Resets text color
-	link.style.backgroundcolor = ""; // Resets background color
+    link.style.color = "";
+    link.style.backgroundColor = "";
 }
