e22c2c72454227109e1f756f00e777f043e63d87
deenorthn <deenorth.naganathan@torontomu.ca>
2024-10-30 21:18:20 -0400

    Lab 6

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function askName() {
 var Name = prompt("Please enter your name", "Name"); // Prompting the user to type in their name
 document.getElementByID("displayName").innerText = "Hello," + Name + "Welcome to my page!";
-if (userName) { // Check if userName is not null or an empty string
-        document.getElementByID("displayName").innerText = "Hello, " + userName + "!"; // Display user's name
+if (Name) { // Check if Name is not null or an empty string
+        document.getElementByID("displayName").innerText = "Hello, " + Name + "!"; // Display user's name
     } else {
         document.getElementByID("displayName").innerText = "Hello!"; // Fallback message if no name was provided
     }
 }
 function toggleImageSize() { // Enlarging the image
 	var image = document.getElementByID("profilePicture");
-	if (image.style.transform = "scale(2)") {
+	if (image.style.transform === "scale(2)") {
 		image.style.transform = "scale(1)";
 	} else {
 		image.style.transform = "scale(2)";
