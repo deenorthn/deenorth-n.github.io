de02a57e5e9dae2609dfd2036990a0e6962ebd7d
deenorthn <deenorth.naganathan@torontomu.ca>
2024-11-06 21:22:44 -0500

    Lab 6

--- main.js
+++ main.js
@@ -29,3 +29,26 @@ function resetLink(link) {
     link.style.color = "";
     link.style.backgroundColor = "";
 }
+
+// Function to clear form fields and localStorage data
+function resetContact() {
+    document.getElementById("contactme").reset(); // Reset form fields
+    localStorage.clear(); // Clear local storage
+    alert("Form has been cleared!");
+}
+
+// Function to store form data in localStorage and notify user
+function storeData() {
+    var name = document.getElementById("fullname").value;
+    var email = document.getElementById("email").value;
+    var cell = document.getElementById("cell").value;
+    var message = document.getElementById("Message").value;
+
+    // Save data to localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("cell", cell);
+    localStorage.setItem("message", message);
+
+    alert("Message Sent!\nThank you for reaching out, I will get back to you as soon as possible.");
+}
